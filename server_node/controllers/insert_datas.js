bfdd489d8f6c7a8f97a6788c20f17d253aa68601
didier <b.didier745@laposte.net>
2021-05-18 11:20:44 +0200

    get rid of accentuated char + construct query file

--- server_node/controllers/insert_datas.js
+++ server_node/controllers/insert_datas.js
@@ -18,10 +18,12 @@ module.exports = function (req, res, next) {
             return wcb(null,wdatas);
         },
 
-        clear_file,
         parse_json,
         get_real,
-        build_query_file,
+        get_genre,
+        get_actors,
+        get_lieu,
+        build_query_file
 
     ],
     function(error,result){
@@ -91,12 +93,75 @@ var get_real = function(wdatas, wcb)
 	var tab_real = []
 	json_datas.map(function(elt)
 	{
-		var real = elt["nom_realisateur"]
-		if(tab_real.indexOf(real) == -1)
+		var real = elt["nom_realisateur"].toLowerCase().trim()
+		real = cleanUpSpecialChars(real);
+
+
+
+		if(real.indexOf(" - ") != -1)
 		{
-			tab_real.push(real);
+			var tab_reals = real.split(" - ");
+			for(var i = 0; i<tab_reals.length; i++)
+			{
+				if(tab_real.indexOf(tab_reals[i].toLowerCase().trim()) == -1)
+				{
+					tab_real.push(tab_reals[i].toLowerCase().trim());
+
+				}
+
+			}
 
 		}
+		else if( real.indexOf(", ") != -1 )
+		{
+			var tab_reals = real.split(", ");
+			for(var i = 0; i<tab_reals.length; i++)
+			{
+				if(tab_real.indexOf(tab_reals[i].toLowerCase().trim()) == -1)
+				{
+					tab_real.push(tab_reals[i].toLowerCase().trim());
+
+				}
+
+			}
+
+		}
+		else if(real.indexOf(" et ") != -1)
+		{
+			var tab_reals = real.split(" et ");
+			for(var i = 0; i<tab_reals.length; i++)
+			{
+				if(tab_real.indexOf(tab_reals[i].toLowerCase().trim()) == -1)
+				{
+					tab_real.push(tab_reals[i].toLowerCase().trim());
+
+				}
+
+			}
+		}
+		else if(real.indexOf("/") != -1)
+		{
+			var tab_reals = real.split("/");
+			for(var i = 0; i<tab_reals.length; i++)
+			{
+				if(tab_real.indexOf(tab_reals[i].toLowerCase().trim()) == -1)
+				{
+					tab_real.push(tab_reals[i].toLowerCase().trim());
+
+				}
+
+			}
+		}
+		else
+		{
+			if(tab_real.indexOf(real) == -1)
+			{
+				tab_real.push(real);
+
+			}
+		}
+
+
 	})
 
 	var query_real = build_prefix()
@@ -107,13 +172,204 @@ var get_real = function(wdatas, wcb)
 
 	}
 
-	query_real += "\n}\n"
-	wdatas.query_real = query_real;
+	query_real += "\n"
+	wdatas.query = query_real;
 
 	return wcb(null, wdatas);
 
 
 }
+
+
+var get_actors = function(wdatas, wcb)
+{
+	var json_datas = wdatas.json_datas;
+	var query = wdatas.query;
+	
+	var tab_actors = []
+
+	json_datas.map(function(elt)
+	{
+		var actors = elt["actors"].split(",");
+		for(var j = 0; j<actors.length; j++)
+		{
+			var actor = actors[j].toLowerCase().trim();
+			actor = cleanUpSpecialChars(actor);
+
+			if(tab_actors.indexOf(actor) == -1)
+			{
+				tab_actors.push(actor);
+			}			
+		}
+	})
+
+
+	for(var i = 0; i<tab_actors.length; i++)
+	{
+		query += insert_acteur(tab_actors[i], i);
+	}
+
+	query += "\n"
+	wdatas.query = query;
+
+	return wcb(null, wdatas);
+
+
+}
+
+
+var get_genre = function(wdatas, wcb)
+{
+	var json_datas = wdatas.json_datas;
+	var query = wdatas.query;
+
+	var tab_genre = []
+
+	json_datas.map(function(elt)
+	{
+		var genres = elt["Genre"].split(",");
+		for(var j = 0; j<genres.length; j++)
+		{
+			var genre = genres[j].toLowerCase().trim();
+			genre = cleanUpSpecialChars(genre);
+			if(tab_genre.indexOf(genre) == -1)
+			{
+				tab_genre.push(genre);
+			}			
+		}
+	})
+
+
+	for(var i = 0; i<tab_genre.length; i++)
+	{
+		query += insert_genre(tab_genre[i], i);
+	}
+
+	query += "\n"
+	wdatas.query = query;
+
+	return wcb(null, wdatas);
+}
+
+
+var get_lieu = function(wdatas, wcb)
+{
+	var json_datas = wdatas.json_datas;
+	var query = wdatas.query;
+	var lieux = {
+
+		"Europe": {
+			"France": {}
+		}
+
+	};
+
+	var France = lieux["Europe"]["France"];
+
+	json_datas.map(function(elt)
+	{
+
+
+		var lieu = elt["adresse_lieu"];
+		var adresse = lieu.split(",")[0]
+		var tab_adresse_row = adresse.split("/");
+		var quartier = "";
+		var ville = "";
+
+		if(lieu.split(",").length == 1)
+		{
+			quartier = "nsp";
+			ville = "paris"
+		}
+		else
+		{
+			quartier = lieu.split(",")[1].trim().split(" ")[0];
+			ville = lieu.split(",")[1].trim().split(" ")[1]
+		}
+
+		ville = ville[0].toUpperCase() + ville.slice(1)
+
+		ville = cleanUpSpecialChars(ville);
+		quartier = cleanUpSpecialChars(quartier);
+
+		
+
+		if( ! France[ville])
+		{
+			France[ville] = {};
+		}
+		
+		if(! France[ville][quartier])
+		{
+			France[ville][quartier] = [];
+		}
+
+		for(var i = 0; i<tab_adresse_row.length; i++)
+		{
+			if(France[ville][quartier].indexOf(tab_adresse_row[i]) == -1)
+			{
+				France[ville][quartier].push(cleanUpSpecialChars(tab_adresse_row[i].toLowerCase().trim()));
+			}
+		}
+		
+	})
+
+	var index_continent = 0;
+	var index_pays = 0;
+	var index_ville = 0;
+	var index_quartier = 0;
+	var index_adresse = 0;
+
+	for (var continent in lieux)
+	{
+
+	  query += ":continent"+index_continent + " rdfs:label \"" + continent + "\".\n" ;
+
+	  for(var pays in lieux[continent])
+	  {
+	  	query += ":pays"+index_pays + " rdfs:label \"" + pays + "\";\n" ;
+	  	query += ":pays"+index_pays + " :seSitueDans :continent"+index_continent +". \n";
+
+	  	for(var ville in lieux[continent][pays])
+	  	{
+	  		query += ":ville"+index_ville +  " rdfs:label \"" + ville + "\";\n" ;
+	  		query += ":ville"+index_pays + " :seSitueDans :pays"+index_pays +". \n";
+	  		
+	  		for(var quartier in lieux[continent][pays][ville])
+  			{
+  				query += ":quartier"+index_quartier +  " rdfs:label \"" + quartier + "\";\n" ;
+  				query += ":quartier"+index_quartier + " :seSitueDans :ville"+index_ville +". \n";
+  				
+  				for(var i = 0; i< lieux[continent][pays][ville][quartier].length; i++ )
+  				{
+  					query += ":adresse"+index_adresse +  " rdfs:label \"" + lieux[continent][pays][ville][quartier][i] + "\";\n" ;
+  					query += ":adresse"+index_adresse + " :seSitueDans :quartier"+index_quartier +". \n";
+
+  					index_adresse += 1
+  				}
+
+  				index_quartier += 1
+  			}
+
+	  		index_ville += 1;
+	  	}
+
+	  	index_pays += 1;
+	  }
+
+	  index_continent += 1;
+
+	}
+
+	query += "\n"
+	wdatas.query = query;
+
+	return wcb(null, wdatas);
+}
+
+
+
+
 
 var each_row = function(wdatas, wcb)
 {
@@ -228,38 +484,23 @@ function insert_realisateur(nom_real, index)
 	return str;
 }
 
-function insert_genre(row)
+function insert_genre(nom_genre, index)
 {
-	return "";
-
-}
-
-function insert_lieu(row)
-{
-	return "";
-}
-
-function insert_actors(row)
-{
-	var str = ""
-	var actors = row["actors"].split(",");
-	for(var i = 0; i<actors.length; i++)
-	{
-		str += insert_acteur(actors[i], row["nom_tournage"]);
-	}
-
+	var str = " :genre"+index+" rdfs:label \""+nom_genre+"\".\n"
 	return str;
 }
 
-function insert_acteur(acteur_name, film_name)
+function insert_acteur(acteur_name, index)
 {
-	return "";
+	var str = " :acteur"+index+" rdfs:label \""+acteur_name+"\".\n"
+	return str;
 }
 
 
 var build_query_file = function(wdatas, wcb)
 {
-	var query_str = wdatas.query_real;
+	var query_str = wdatas.query;
+	query_str += "\n}\n";
 
 	fs.writeFile("query_str.txt", query_str, function(err, result)
 	{
@@ -271,4 +512,14 @@ var build_query_file = function(wdatas, wcb)
 
 	})
 
+}
+
+
+function cleanUpSpecialChars(str)
+{
+    return str
+        .replace(/[ÀÁÂÃÄÅ]/g,"A")
+        .replace(/[àáâãäå]/g,"a")
+        .replace(/[ÈÉÊË]/g,"E")
+        .replace(/[èé]/g,"e")
 }
